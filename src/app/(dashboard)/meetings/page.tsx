d3d3e939e067e530b5abc83942f56fc081c13f52
Nicksm98 <nicholas.sanmartin20@gmail.com>
2025-06-18 00:45:26 -0400

    Meeting-Form

--- src/app/(dashboard)/meetings/page.tsx
+++ src/app/(dashboard)/meetings/page.tsx
@@ -1,20 +1,34 @@
-import { Suspense } from 'react'
-import { ErrorBoundary } from 'react-error-boundary'
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query'
+import { Suspense } from "react"
+import { headers } from "next/headers"
+import { redirect } from "next/navigation"
+import { ErrorBoundary } from "react-error-boundary"
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query"
 
-import { getQueryClient, trpc } from '@/trpc/server'
+import { auth } from "@/lib/auth"
+import { getQueryClient, trpc } from "@/trpc/server"
 
+import { MeetingsListHeader } from "@/modules/meetings/ui/components/meetings-list-header"
 import { 
-    MeetingsView, 
-    MeetingsViewError, 
-    MeetingsViewLoading 
-}   from '@/modules/meetings/ui/views/meeting-view'
+  MeetingsView, 
+  MeetingsViewError, 
+  MeetingsViewLoading 
+}   from "@/modules/meetings/ui/views/meeting-view"
 
-const Page = () => {
+const Page = async () => {
+  const session = await auth.api.getSession({
+        headers: await headers()
+      })
+    
+      if (!session) {
+        redirect("/sign-in")
+      }
+
   const queryClient = getQueryClient()
   void queryClient.prefetchQuery(trpc.meetings.getMany.queryOptions({}))
 
   return (
+    <>
+    <MeetingsListHeader />
     <HydrationBoundary state={dehydrate(queryClient)}>
       <Suspense fallback={<MeetingsViewLoading />}>
         <ErrorBoundary fallback={<MeetingsViewError />}>
@@ -22,6 +36,7 @@ const Page = () => {
         </ErrorBoundary>
       </Suspense>
     </HydrationBoundary>
+    </>
   )
 }
 
